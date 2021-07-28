8baf22099ea7eafc4f28c69f7d1640eb6a53d51f
PrajwalCH <prajjwal2058@gmail.com>
2021-07-28 16:40:06 +0545

    refactor: rename encoder and decoder arg variables name
    encoder: reply_buffer to buffer, msg to msg_data
    decoder: encoded_msg_buff to encoded_msg

--- src/encode_decode.c
+++ src/encode_decode.c
@@ -10,23 +10,22 @@
 #include <string.h>
 
 bool
-encode_msg(char *reply_buffer,
+encode_msg(char *buffer,
            int buffer_size,
            int msg_id,
-           const char *msg)
+           const char *msg_data)
 {
-    int si= snprintf(reply_buffer, buffer_size, "[%d,\"%s\"]", msg_id, msg);
-    printf("%i\n", si);
+    snprintf(buffer, buffer_size, "[%d,\"%s\"]", msg_id, msg_data);
     return true;
 }
 
 bool
-decode_msg(char *encoded_msg_buff, struct DecodedMsg *decoded_msg)
+decode_msg(char *encoded_msg, struct DecodedMsg *decoded_msg)
 {
     decoded_msg->msg_id = 0;
     memset(&decoded_msg->msg_data, 0, sizeof(decoded_msg->msg_data));
 
-    if (sscanf(encoded_msg_buff, "[%d,\"%[^\"]]", &(decoded_msg->msg_id), decoded_msg->msg_data) != 2) {
+    if (sscanf(encoded_msg, "[%d,\"%[^\"]]", &(decoded_msg->msg_id), decoded_msg->msg_data) != 2) {
         fprintf(stderr, "fail to decode the message\n");
         return false;
     }
